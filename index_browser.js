df0c6c8953570c39ef623f3403b1e8064b31ab65
shynome <shynome@gmail.com>
2023-06-02 02:47:33 +0800

    带有 browser polyfill

--- index_browser.js
+++ index_browser.js
@@ -1,20 +1,2 @@
 require("./go_wasm_js/wasm_exec");
-
-const go = new Go();
-
-if (!WebAssembly.instantiateStreaming) {
-  WebAssembly.instantiateStreaming = async (resp, importObject) => {
-    const source = await (await resp).arrayBuffer();
-    return await WebAssembly.instantiate(source, importObject);
-  };
-}
-
-exports.XheConnectInit = async (
-  wasmUrl = "https://unpkg.com/xhe-wc/xhe-wc.wasm"
-) => {
-  const { instance } = await WebAssembly.instantiateStreaming(
-    fetch(wasmUrl),
-    go.importObject
-  );
-  return { process: go.run(instance) };
-};
+module.exports = require("./index.cjs");
