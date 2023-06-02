d2cff6440397e858b5bd90d73d0128858084eb25
shynome <shynome@gmail.com>
2023-06-02 14:46:59 +0800

    不再转成 data url base64, 因为过于浪费内存

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: "browser.mjs",
+      entry: ".",
       name: "XheWC",
       formats: ["umd"],
     },
